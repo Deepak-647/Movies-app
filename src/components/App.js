29ecc9f2c369e0a039d94b4a7f6c4d202cb96969
Deepak <dbehera647@gmail.com>
2023-02-10 12:31:12 +0530

    Adding UI and components to our Movie app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-
-
+import {data} from '../files/data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 function App() {
   return (
     <div className="App">
-      Setting up the project
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+      <div className="list">
+{data.map(movie =>(
+  <MovieCard movie={movie}/>
+))}
+        </div>  
+      </div>
     </div>
   );
 }
