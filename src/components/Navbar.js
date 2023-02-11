c924659e228cb715d231752ffe23e5eeb05940a7
Deepak <dbehera647@gmail.com>
2023-02-11 18:54:28 +0530

    Adding action and reducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,10 @@ class Navbar extends React.Component {
     render(){
         return (
             <div className="nav">
-              <div className="search-container"></div>
+              <div className="search-container">
               <input/>
               <button id="search-btn">Search</button>
+              </div>
             </div>
           );
     }
