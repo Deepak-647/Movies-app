394767f702b970f5f72b7feb6ee1479f8b03322a
Deepak <dbehera647@gmail.com>
2023-02-11 20:31:56 +0530

    Adding store to our app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('STORE',store);
+console.log('STATE',store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
