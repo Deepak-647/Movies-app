eee184a2722173198a83132b68d37caec06e3c4e
Deepak <dbehera647@gmail.com>
2023-02-15 12:41:04 +0530

    Combining Reducers

--- src/components/App.js
+++ src/components/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
     
   }
   isMovieFavourite = (movie) =>{
-    const {favourites} =this.props.store.getState();
-    const index =favourites.indexOf(movie);
+    const {movies} =this.props.store.getState();
+    const index =movies.favourites.indexOf(movie);
     if (index !== -1){
       //found the movie
       return true;
@@ -29,7 +29,8 @@ class App extends React.Component {
     this.props.store.dispatch(setShowFavourites(val))
   }
   render (){
-    const {list,favourites ,showFavourites} = this.props.store.getState();
+    const {movies} =this.props.store.getState();
+    const {list,favourites ,showFavourites} = movies;
     console.log("RENDER",this.props.store.getState());
 
     const displayMovies =showFavourites ? favourites : list;
