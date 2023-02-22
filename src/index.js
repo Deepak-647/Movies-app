7943e9f127911b1a677d46a5730f1033ff6f869e
Deepak <dbehera647@gmail.com>
2023-02-22 16:19:03 +0530

    Using Context API -1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
@@ -41,6 +41,10 @@ const store = createStore(rootReducer ,applyMiddleware(logger,thunk));
 console.log('STORE',store);
 // console.log('BEFORE STATE',store.getState());
 
+
+export const StoreContext = createContext();
+
+console.log("StoreContext",StoreContext);
 // store.dispatch({
 //   type : "ADD_MOVIES",
 //   movies: [{name:"Superman"}]
@@ -48,13 +52,24 @@ console.log('STORE',store);
 
 // console.log('AFTER STATE',store.getState());
 
+class Provider extends React.Component{
+  render(){
+    const {store} =this.props;
+return <StoreContext.Provider value={store}>
+  {this.props.children}
+</StoreContext.Provider>
+  }
+}
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
-    <App store={store} />
+    <App />
   </React.StrictMode>
+  </Provider>
 );
 
 
